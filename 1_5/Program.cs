1a073f292b6020416217727992750c196bf28e95
Technopriest90 <erokhin.ag@yandex.ru>
2017-01-24 15:05:25 +0300

    FULL

--- 1_5/Program.cs
+++ 1_5/Program.cs
@@ -36,137 +36,179 @@
 //         Console.ReadKey();
 //     }
 // }
-    // class Comma //использовать запятые в операторе цикла for для выявления наименьшего и наибольшего множителя числа.
+// class Comma //использовать запятые в операторе цикла for для выявления наименьшего и наибольшего множителя числа.
+// {
+//     static void Main()
+//     {
+//         int i, j;
+//         int smallest, largest;
+//         int num;
+// 
+//         num = 11;
+// 
+//         smallest = largest = 1;
+// 
+//         for (i = 2, j = num / 2; (i <= num / 2) & (j >= 2); i++, j--)
+//         {
+//             if ((smallest == 1) & ((num % i) == 0))
+//                 smallest = i;
+// 
+//             if ((largest == 1) & ((num % j) == 0))
+//                 largest = j;
+//         }
+//         Console.WriteLine("Наибольший множитель: " + largest);
+//         Console.WriteLine("Наименьший множитель: " + smallest);
+//         Console.ReadKey();
+//     }
+// }
+// 
+//     class Empty3 //тело цикла может быть пустым
+// {
+//     static void Main()
+//     {
+//         int i;
+//         int sum = 0;
+// 
+//         //получить сумму чисел от 1 до 5
+//         for (i = 1; i <= 5; sum += i++);
+// 
+//         Console.WriteLine("Сумма равна " + sum);
+//         Console.ReadKey();
+//     }
+// }
+// 
+//     class ForVar
+// {
+//     static void Main() {
+//         int sum = 0;
+//         int fact = 1;
+// 
+//         //вычислить факториал чисел от 1 до 5
+//         for(int i = 1; i <= 5; i++)
+//         {
+//             sum += i; //переменная i действует в цикле
+//             fact *= i;
+//         }
+// 
+//         //а здесь переменная i недоступна
+//         Console.WriteLine("Сумма равна " + sum);
+//         Console.WriteLine("Факториал равен " + fact);
+//         Console.ReadKey();
+//     }
+// }
+// 
+//     class WhileDemo // ВЫчислить порядок величины целого числа
+// {
+//     static void Main()
+//     {
+//         int num;
+//         int mag;
+// 
+//         num = 435679;
+//         mag = 0;
+// 
+//         Console.WriteLine("Число: " + num);
+// 
+//         while(num > 0)
+//         {
+//             mag++;
+//             num = num / 10;
+//         }
+// 
+//         Console.WriteLine("Порядок величины: " + mag);
+//         Console.ReadKey();
+//     }
+// }
+// 
+//     class Power // вычислить целые степени числа 2
+// {
+//     static void Main()
+//     {
+//         int e;
+//         int result;
+// 
+//         for (int i=0; i < 10; i++)
+//         {
+//             result = i;
+//             e = i;
+// 
+//             while (e > 0)
+//             {
+//                 result *= 2;
+//                 e--;
+//             }
+// 
+//             Console.WriteLine("2 в степени " + i + " равно " + result);
+//             
+//         }
+//         Console.ReadKey();
+//     }
+// }
+
+//     class DoWhileDemo //отобразить цифры целого числа в обратном порядке
+// {
+//     static void Main ()
+//     {
+//         int num;
+//         int nextdigit;
+// 
+//         num = 198;
+// 
+//         Console.WriteLine("Число: " + num);
+// 
+//         Console.Write("Число в обратно порядке: ");
+// 
+//         do
+//         {
+//             nextdigit = num % 10;
+//             Console.Write(nextdigit);
+//             num = num / 10;
+//         } while (num > 0);
+// 
+//         Console.WriteLine();
+//         Console.ReadKey();
+//     }
+// }
+    // 
+    // class FindSmallestFactor //выявить наименьший множитель числа.
     // {
     //     static void Main()
     //     {
-    //         int i, j;
-    //         int smallest, largest;
-    //         int num;
+    //         int factor = 1;
+    //         int num = 1000;
     // 
-    //         num = 11;
-    // 
-    //         smallest = largest = 1;
-    // 
-    //         for (i = 2, j = num / 2; (i <= num / 2) & (j >= 2); i++, j--)
+    //         for (int i = 2; i <= num / i; i++)
     //         {
-    //             if ((smallest == 1) & ((num % i) == 0))
-    //                 smallest = i;
-    // 
-    //             if ((largest == 1) & ((num % j) == 0))
-    //                 largest = j;
-    //         }
-    //         Console.WriteLine("Наибольший множитель: " + largest);
-    //         Console.WriteLine("Наименьший множитель: " + smallest);
-    //         Console.ReadKey();
-    //     }
-    // }
-    // 
-    //     class Empty3 //тело цикла может быть пустым
-    // {
-    //     static void Main()
-    //     {
-    //         int i;
-    //         int sum = 0;
-    // 
-    //         //получить сумму чисел от 1 до 5
-    //         for (i = 1; i <= 5; sum += i++);
-    // 
-    //         Console.WriteLine("Сумма равна " + sum);
-    //         Console.ReadKey();
-    //     }
-    // }
-    // 
-    //     class ForVar
-    // {
-    //     static void Main() {
-    //         int sum = 0;
-    //         int fact = 1;
-    // 
-    //         //вычислить факториал чисел от 1 до 5
-    //         for(int i = 1; i <= 5; i++)
-    //         {
-    //             sum += i; //переменная i действует в цикле
-    //             fact *= i;
-    //         }
-    // 
-    //         //а здесь переменная i недоступна
-    //         Console.WriteLine("Сумма равна " + sum);
-    //         Console.WriteLine("Факториал равен " + fact);
-    //         Console.ReadKey();
-    //     }
-    // }
-    // 
-    //     class WhileDemo // ВЫчислить порядок величины целого числа
-    // {
-    //     static void Main()
-    //     {
-    //         int num;
-    //         int mag;
-    // 
-    //         num = 435679;
-    //         mag = 0;
-    // 
-    //         Console.WriteLine("Число: " + num);
-    // 
-    //         while(num > 0)
-    //         {
-    //             mag++;
-    //             num = num / 10;
-    //         }
-    // 
-    //         Console.WriteLine("Порядок величины: " + mag);
-    //         Console.ReadKey();
-    //     }
-    // }
-    // 
-    //     class Power // вычислить целые степени числа 2
-    // {
-    //     static void Main()
-    //     {
-    //         int e;
-    //         int result;
-    // 
-    //         for (int i=0; i < 10; i++)
-    //         {
-    //             result = i;
-    //             e = i;
-    // 
-    //             while (e > 0)
+    //             if ((num % i) == 0)
     //             {
-    //                 result *= 2;
-    //                 e--;
+    //                 factor = i;
+    //                 break; //прервать цикл, как только будет выявлен наименьший множитель числа
     //             }
-    // 
-    //             Console.WriteLine("2 в степени " + i + " равно " + result);
-    //             
     //         }
+    //         Console.WriteLine("Наименьший множитель равен " + factor);
     //         Console.ReadKey();
-    //     }
+    //             }
     // }
-
-    //     class DoWhileDemo //отобразить цифры целого числа в обратном порядке
+    // 
+    //     class Use_goto
     // {
-    //     static void Main ()
+    //     static void Main()
     //     {
-    //         int num;
-    //         int nextdigit;
+    //         int i = 0, j = 0, k = 0;
     // 
-    //         num = 198;
-    // 
-    //         Console.WriteLine("Число: " + num);
-    // 
-    //         Console.Write("Число в обратно порядке: ");
-    // 
-    //         do
+    //         for(i=0; i < 10; i++)
     //         {
-    //             nextdigit = num % 10;
-    //             Console.Write(nextdigit);
-    //             num = num / 10;
-    //         } while (num > 0);
-    // 
-    //         Console.WriteLine();
+    //             for (j=0; j < 10; j++)
+    //             {
+    //                 for(k=0; k < 10; k++)
+    //                 {
+    //                     Console.WriteLine("i, j, k: " + i + " " + j + " " + k);
+    //                     if (k == 3) goto stop;
+    //                 }
+    //             }
+    //         }
+    //     stop:
+    //         Console.WriteLine("Остановлено! i, j, k: " + i + " " + j + " " + k);
     //         Console.ReadKey();
     //     }
     // }
-
